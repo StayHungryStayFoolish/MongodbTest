2594f6a6cd530deb961504973333f36ddce3e2eb
StayHungryStayFoolish <bonismo@hotmail.com>
2017-10-23 18:40:42 +0800

    commit

--- Mongodb/src/main/java/MongoDBJDBC.java
+++ Mongodb/src/main/java/MongoDBJDBC.java
@@ -1,5 +1,7 @@
 import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 
 /**
  * Created by bonismo@hotmail.com
@@ -7,8 +9,22 @@ import com.mongodb.client.MongoDatabase;
  */
 public class MongoDBJDBC {
     public static void main(String[] args) {
-        MongoClient mongoClient = new MongoClient("10.211.557",27017);
-        MongoDatabase mongoDatabase = mongoClient.getDatabase("mycol");
-        System.out.println("成功连接数据库");
+        try {
+            MongoClient mongoClient = new MongoClient("10.211.55.7", 27017);
+            MongoDatabase mongoDatabase = mongoClient.getDatabase("mycol");
+            System.out.println("成功连接数据库");
+
+//            创建集合
+//            mongoDatabase.createCollection("test");
+//            System.out.println("集合创建成功");
+
+            // 获取集合
+            MongoCollection<Document> collection = mongoDatabase.getCollection("test");
+            System.out.println(" test 集合选择成功");
+
+
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ":" + e.getMessage());
+        }
     }
 }
