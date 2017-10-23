b5723e0f2bea427e5f89392d09192b09d1a8f1b6
StayHungryStayFoolish <bonismo@hotmail.com>
2017-10-23 18:58:36 +0800

    commit

---
+++ Mongodb/src/main/java/FindCollection.java
@@ -0,0 +1,6 @@
+/**
+ * Created by bonismo@hotmail.com
+ * 下午6:58 on 17/10/23.
+ */
+public class FindCollection {
+}
--- Mongodb/src/main/java/MongoDBJDBC.java
+++ Mongodb/src/main/java/MongoDBJDBC.java
@@ -3,6 +3,9 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by bonismo@hotmail.com
  * 下午6:17 on 17/10/23.
@@ -22,7 +25,23 @@ public class MongoDBJDBC {
             MongoCollection<Document> collection = mongoDatabase.getCollection("test");
             System.out.println(" test 集合选择成功");
 
-
+            /**
+             * 插入文档
+             * 1.创建 org.bason.Document 参数 K-V
+             * 2.创建插入的文档集合 List<Document>
+             * 3.将文档集合插入数据库集合中
+             *   mongoCollection.insertMany(List<Document>)
+             *
+             *   插入单个文档可以用
+             *   mongoCollection.insertOne(Document)
+             */
+            Document document = new Document("title", "MongoDB")
+                    .append("description", "database")
+                    .append("likes", 100);
+            List<Document> documents = new ArrayList<Document>();
+            documents.add(document);
+            collection.insertMany(documents);
+            System.out.println("文档插入成功");
         } catch (Exception e) {
             System.err.println(e.getClass().getName() + ":" + e.getMessage());
         }
