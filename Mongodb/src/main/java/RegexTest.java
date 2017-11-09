5817653e9b0c39bb8a58cb4358ba6c8a2b97b940
StayHungryStayFoolish <bonismo@hotmail.com>
2017-11-10 01:27:15 +0800

    commit

--- Mongodb/src/main/java/RegexTest.java
+++ Mongodb/src/main/java/RegexTest.java
@@ -13,44 +13,34 @@ public class RegexTest {
 
     public static void main(String[] args) {
         String template = "【$!{data.channelName}】您预订的直播：《$!{data.title}-$!{data.name}》，将于30分钟后开始。如有疑问，请添加客服微信：jiaoyinkefu 进行反馈。";
+        String[] after = {"直播课堂", "课堂名字", "平安保险"};
         String regex = "\\$!\\{[^}]*\\}";
+        for (int i = 0; i < after.length; i++) {
+            if (template.contains("$!{data.channelName}")) {
+                String demo = template.replaceAll("\\$!\\{data.channelName}", after[i]);
+//                System.out.println(demo);
+            }
+        }
+
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(template);
         while (matcher.find()) {
-            System.out.println(matcher.group(0));
-        }
-
-        if (template.indexOf("\n") != -1) {
-            String[] tempArray = template.split("\n");
-            int kwCount = 1;
-            if (tempArray.length > 2) {
-                kwCount = tempArray.length - 2;
-            }
-            List<Map<String, String>> contentList = new LinkedList<Map<String, String>>();
-            for (String s : tempArray) {
-                Map<String, String> map = new HashMap<String, String>();
-                map.put("value", s);
-                if (s.indexOf("：") != -1) {
-                    String[] temp = s.split("：");
-                    String tempStr = temp[1];
-                    tempStr = tempStr.replace("}", "");
-                    tempStr = tempStr.replace("\\{", "");
-                    tempStr = tempStr.replace(".", "");
-                    map.put("key", tempStr);
+            String before = matcher.group(0);
+            for (int i = 0; i < after.length; i++) {
+                if (template.contains(before)) {
+                    String demo = template.replaceAll("\\$!\\{data.channelName}", after[i]);
+                    System.out.println("==>"+demo);
                 }
-                contentList.add(map);
             }
-            System.out.println("ContentList"+contentList.toArray());
-
         }
     }
 }
 
 /**
- $!{data.channelName}
- class java.lang.String
- $!{data.title}
- class java.lang.String
- $!{data.name}
- class java.lang.String
+ * $!{data.channelName}
+ * class java.lang.String
+ * $!{data.title}
+ * class java.lang.String
+ * $!{data.name}
+ * class java.lang.String
  */
