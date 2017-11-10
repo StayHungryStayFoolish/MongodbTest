b198fd2aa97a1fd480165ec08f12154dc25a5dbe
StayHungryStayFoolish <bonismo@hotmail.com>
2017-11-10 10:43:16 +0800

    commit

--- Mongodb/src/main/java/TemplateReplace.java
+++ Mongodb/src/main/java/TemplateReplace.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -9,9 +11,44 @@ import java.util.regex.Pattern;
  */
 public class TemplateReplace {
 
+    public static void main(String[] args) throws Exception {
+        HashMap<String, String> map = new HashMap<String, String>();
+        String temp = "【$!{data.channelName}】您预订的直播：《$!{data.title}-$!{data.name}》，将于30分钟后开始。...";
+        map = composeTemplate(temp, "data.channelName", "平安");
+//        System.out.println(composeMessage(temp,map));
 
+        List list = new ArrayList();
+        list = getKey(temp);
+        for (Object o : list) {
+            System.out.println("==>"+o);
+        }
+    }
 
+    /**
+     * 获取模板中的占位符，即 Map 中的 Key
+     * @param template
+     * @return
+     */
+    public static List<String> getKey(String template) {
+        List<String> list = new ArrayList<String>();
+        String regex = "\\$!\\{(.+?)\\}";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(template);
+        while (matcher.find()) {
+            String name = matcher.group(1);
+            list.add(name);
+        }
+        return list;
+    }
 
+    /**
+     * 根据 Key 更新 Map 值
+     * 替换占位符内容
+     * @param template
+     * @param key
+     * @param value
+     * @return
+     */
     public static HashMap<String,String> composeTemplate(String template, String key, String value) {
         HashMap<String,String> map = new HashMap();
         String regex = "\\$!\\{(.+?)\\}";
@@ -26,6 +63,13 @@ public class TemplateReplace {
         return map;
     }
 
+    /**
+     * 将 Map 的值更新到模板中，替换到原来的占位符
+     * @param template
+     * @param data
+     * @return
+     * @throws Exception
+     */
     public static String composeMessage(String template, Map data)
             throws Exception {
         String regex = "\\$!\\{(.+?)\\}";
